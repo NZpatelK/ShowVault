0a64c1012d45229dfe7e43b368d83205df38f686
NZpatelK <patelk.dev@hotmail.com>
2024-08-23 22:33:16 +1200

    update new card

--- src/app/api/movies/route.js
+++ src/app/api/movies/route.js
@@ -7,7 +7,97 @@ const baseUrl = 'https://api.themoviedb.org/3';
 export async function GET(req) {
     noStore();
     const { searchParams } = req.nextUrl;
-    const filterType = searchParams.get('filterType');
+
+    if (searchParams.get('logo')) {
+        return getImage(searchParams.get('logo'));
+    }
+    else {
+        const filterType = searchParams.get('filterType');
+        return getList(filterType);
+    }
+
+
+    // const endpoint = `movie/${filterType}`;
+
+    // const response = await axios.get(`${baseUrl}/${endpoint}`, {
+    //     params: { api_key: apiKey, language: 'en-US' },
+    //     responseType: 'json',
+    // });
+
+    // const { results } = response.data;
+    // const body = JSON.stringify(results);
+
+    // return new Response(body, {
+    //     headers: { 'Content-Type': 'application/json' },
+    // });
+}
+
+const getImage = async (movieId) => {
+    let logo = undefined;
+    let poster = undefined;
+
+    const logoResponse = await axios.get(`${baseUrl}/movie/${movieId}/images`, {
+        params: { api_key: apiKey},
+        responseType: 'json',
+    });
+
+    logo = logoResponse.data.logos.filter(logo => logo.iso_639_1 === 'en');
+
+    const response = await axios.get(`${baseUrl}/movie/${movieId}/images`, {
+        params: { api_key: apiKey},
+        responseType: 'json',
+    });
+
+    poster = response.data.posters.filter(poster => poster.iso_639_1 === null);
+
+
+    const preBody = {
+        logo: logo[0]?.file_path || null,
+        poster: poster[0]?.file_path || null
+    };
+
+    const body = JSON.stringify(preBody);
+
+    return new Response(body, {
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
+
+
+// const getLogo = async (movieId) => {
+
+//     const response = await axios.get(`${baseUrl}/movie/${movieId}/images`, {
+//         params: { api_key: apiKey},
+//         responseType: 'json',
+//     });
+
+//     const engLogo = response.data.logos.filter(logo => logo.iso_639_1 === 'en');
+
+    
+//     const body = JSON.stringify(engLogo[0].file_path);
+//     return new Response(body, {
+//         headers: { 'Content-Type': 'application/json' },
+//     });
+
+// };
+
+// const getPoster = async(movieId) => {
+//     const response = await axios.get(`${baseUrl}/movie/${movieId}/images`, {
+//         params: { api_key: apiKey},
+//         responseType: 'json',
+//     });
+
+//     const nonTitlePoster = response.data.posters.filter(poster => poster.iso_639_1 === null);
+
+//     const body = JSON.stringify(nonTitlePoster[0].file_path);
+
+//     return new Response(body, {
+//         headers: { 'Content-Type': 'application/json' },
+//     });
+// }
+
+const getList = async(filterType) => {
+
     const endpoint = `movie/${filterType}`;
 
     const response = await axios.get(`${baseUrl}/${endpoint}`, {
@@ -21,7 +111,8 @@ export async function GET(req) {
     return new Response(body, {
         headers: { 'Content-Type': 'application/json' },
     });
+
 }
 
-//// https://api.themoviedb.org/3/movie/${movie_id}/images?api_key=${env.apikey}
+// https://api.themoviedb.org/3/movie/${movie_id}/images?api_key=${env.apikey}
 // https://image.tmdb.org/t/p/original/9xIpw0fws1OkMCBzHZh0PEZdgg9.png
--- src/app/components/ShowCard.js
+++ src/app/components/ShowCard.js
@@ -1,17 +1,43 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Image from 'next/image';
 import '@/app/styles/ShowCard.css';
 
 export default function ShowCard({ movie }) {
-  
+
+  const [isLogoDisplayed, setIsLogoDisplayed] = useState(true);
+  const [logo, setLogo] = useState(null);
+  const [poster, setPoster] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from the API route using Axios
+    axios.get('/api/movies', {
+      params: {
+        logo: movie.id
+      },
+    })
+      .then((response) => {
+        setLogo(response.data.logo);
+        setPoster(response.data.poster);
+        setIsLogoDisplayed(true);
+      })
+      .catch((error) => {
+        setError(error);
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className="card">
       <div className='poster'>
-        <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} width={500} height={700} />
+        <Image src={`https://image.tmdb.org/t/p/w500${poster || movie.poster_path}`} alt={movie.title} width={500} height={700} />
       </div>
       <div className='content'>
-        {/* <h2>{movie.title}</h2>
-        <p>{movie.overview}</p>
-        <p>Rating: {movie.vote_average}</p> */}
+        {isLogoDisplayed ? <Image src={`https://image.tmdb.org/t/p/original${logo}`} alt={movie.title} width={300} height={300} onError={() => setIsLogoDisplayed(false)} /> : null}
+        {/* <h2>{movie.title}</h2> */}
+        {/* <p>{movie.overview}</p> */}
+        {/* <p>Rating: {movie.vote_average}</p> */}
       </div>
     </div>
   );
